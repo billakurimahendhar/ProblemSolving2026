c4b0340227f33108d44ef1a0658b4f03e3568095
mahendharbillakuri <billakurimahendhar@gmail.com>
2026-01-27 20:09:47 +0530

    hashmap

---
+++ FindTheDifference.java
@@ -0,0 +1,19 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        HashMap<Character,Integer>map=new HashMap<>();
+       for(int i=0;i<s.length();i++){
+          map.put(s.charAt(i),map.getOrDefault(s.charAt(i),0)+1);
+       }
+       for(int i=0;i<t.length();i++){
+          map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)-1);
+          if(map.get(t.charAt(i))==0)
+          map.remove(t.charAt(i));
+       }
+       char c=' ';
+       for(char i:map.keySet()){
+        c=i;
+        break;
+       }
+       return c;
+    }
+}
