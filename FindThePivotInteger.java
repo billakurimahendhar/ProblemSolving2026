78960373d3752d4ccafa3a600188ae0b30c4c1e3
mahendharbillakuri <billakurimahendhar@gmail.com>
2026-01-29 11:29:25 +0530

    prefix sum

---
+++ FindThePivotInteger.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int a[]=new int[n+1];
+        int b[]=new int[n+1];
+        a[1]=1;
+        for(int i=2;i<=n;i++)
+        a[i]=a[i-1]+i;
+        b[n]=n;
+        for(int i=n-1;i>=1;i--)
+        b[i]=b[i+1]+i;
+        for(int i=1;i<=n;i++)
+        if(a[i]==b[i])
+        return i;
+   return -1;
+    }
+}
