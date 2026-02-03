20072b75e824ee37f0e1476d61017ef5d3839aa1
mahendharbillakuri <billakurimahendhar@gmail.com>
2026-02-03 20:25:59 +0530

    HashSet

---
+++ MaximumSubstringsWithDistinctStart.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int maxDistinct(String s) {
+        HashSet<Character>set=new HashSet<Character>();
+        for(int i=0;i<s.length();i++)
+        set.add(s.charAt(i));
+        return set.size();
+    }
+}
