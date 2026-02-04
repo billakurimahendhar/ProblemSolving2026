0c79414f826260b9b38edd4671b52ae26ccf851d
mahendharbillakuri <billakurimahendhar@gmail.com>
2026-02-04 21:20:22 +0530

    prefix sum

---
+++ SumOfAllOddLengthSubArrays.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+         int sum=0;
+   
+    for(int i=0;i<arr.length;i++){
+        int ans=0;
+        int count=0;
+         for(int j=i;j<arr.length;j++){
+          ans+=arr[j];
+          count++;
+          if(count%2!=0){
+            sum+=ans;
+          }  
+
+         }
+        
+    } 
+    return sum; 
+    }
+}
