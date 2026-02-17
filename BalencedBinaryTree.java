2706d9d0576404e2d3b470f23cbb5e777e506f51
mahendharbillakuri <billakurimahendhar@gmail.com>
2026-02-17 19:03:06 +0530

    trees

---
+++ BalencedBinaryTree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        int hl=height(root.left);
+        int hr=height(root.right);
+        if(Math.abs(hl-hr)>1)
+        return false;
+        return isBalanced(root.left) && isBalanced(root.right);
+
+    }
+     private int height(TreeNode node) {
+        if (node == null) return 0;
+
+        return 1 + Math.max(height(node.left), height(node.right));
+    }
+}
