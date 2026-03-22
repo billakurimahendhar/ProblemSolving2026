2390e95313615a869825b6c4c2c37bed33d7a73b
mahendharbillakuri <billakurimahendhar@gmail.com>
2026-03-22 21:57:29 +0530

    hashmap

---
+++ LongestHarmoniousSubsequence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i:nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        int max=0;
+        for(int i:nums){
+            if(map.containsKey(i+1)){
+                
+                System.out.println(map.get(i));
+                max=Math.max(max,map.get(i)+map.get(i+1));
+                System.out.println(max);
+            }
+        }
+        return max;
+    }
+}
