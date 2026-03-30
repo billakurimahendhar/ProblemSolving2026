d4bb50ffa6cff229e3d817d81979d7c521193b52
mahendharbillakuri <billakurimahendhar@gmail.com>
2026-03-30 19:52:03 +0530

    xor

---
+++ SingleNumber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] singleNumber(int[] nums) {
+        int xor=0;
+        for(int num:nums)
+        xor^=num;
+        int set=xor&-xor;
+        int group1=0,group2=0;
+        for(int num:nums){
+            if((num&set)!=0)
+            group1^=num;
+            else
+            group2^=num;
+        }
+        return new int[]{group1,group2};
+    }
