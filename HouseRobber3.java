a80e9e89cb07492a146532296d773469bbc76398
mahendharbillakuri <billakurimahendhar@gmail.com>
2026-04-06 17:47:03 +0530

    tree dp

---
+++ HouseRobber3.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int[] f(TreeNode root){
+        if(root==null)
+        return new int[2];
+        int []left=f(root.left);
+        int []right=f(root.right);
+        int []res=new int[2];
+        res[0]=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        res[1]=root.val+left[0]+right[0];
+        return res;
+
+    }
+    public int rob(TreeNode root) {
+        int res[]=f(root);
+        return Math.max(res[0],res[1]);
+    }
+}
