d2d4749ca58b64140ecfdd62a750b42705bcd846
mahendharbillakuri <billakurimahendhar@gmail.com>
2026-04-10 20:51:53 +0530

    hashmap

---
+++ MinimumDistanceBetweenThreeElements1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minimumDistance(int[] nums) {
+        HashMap<Integer,List<Integer>>map=new HashMap<>();
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            map.putIfAbsent(nums[i],new ArrayList<Integer>());
+            map.get(nums[i]).add(i);
+            if(map.get(nums[i]).size()==3){
+               int a=map.get(nums[i]).get(0);
+               int b=map.get(nums[i]).get(1);
+               int c=map.get(nums[i]).get(2);
+               min=Math.min(min,Math.abs(a-b)+Math.abs(a-c)+Math.abs(b-c));
+               map.get(nums[i]).remove(0);
+            }
+        }
+            return min==Integer.MAX_VALUE?-1:min;
+    }
+}
